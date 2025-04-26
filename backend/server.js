abe579b9487f88f295d56d6a6e643d0d6a22a419
Radhika Chidrawra <chidrawarradhika@gmail.com>
2025-04-26 13:50:55 +0530

    Update server.js
    deployed frontend

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO
 const io = new Server(server, {
   cors: {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['https://curapoints.onrender.com/', 'https://curapoints-main.onrender.com/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true
   }
